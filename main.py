5155c8a05ae825e6b342633c91cadf1ab2431205
Lud7mila <foxnlo77@gmail.com>
2024-10-02 18:16:58 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+print("1st program ", (9 ** (1/2)) * 5)
+
+print("2nd program ", (9.99 > 9.98) and (1000 != 1000.1))
+
+a = 2 * 2 + 2
+b = 2 * (2 + 2)
+print("3rd program ", a, b, a == b)
+
+print("4th program ", int(float('123.456') * 10) % 10 )
